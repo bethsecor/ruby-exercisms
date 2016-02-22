9520d574946a26e3b82736bb15c06d43f5fcdfe3
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-21 21:33:24 -0700

    difference of squares complete

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,20 @@
+class Squares
+  VERSION = 1
+  attr_reader :numbers
+
+  def initialize(number)
+    @numbers = (1..number).to_a
+  end
+
+  def square_of_sums
+    numbers.reduce(&:+)**2
+  end
+
+  def sum_of_squares
+    numbers.map { |n| n**2 }.reduce(&:+)
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+end
